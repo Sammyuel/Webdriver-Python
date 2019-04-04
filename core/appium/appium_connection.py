c423266bfef737f97f12bea80bbb40a967321c59
Sam Lee <sam49lee@gmail.com>
2019-04-04 17:47:15 -0400

    first commit

---
+++ core/appium/appium_connection.py
@@ -0,0 +1,27 @@
+from appium import webdriver
+
+caps = {}
+caps["platformName"] = "Android"
+caps["platformVersion"] = "8.0.0"
+caps["udid"] = "988e163831454c3134"
+caps["deviceName"] = "Galaxy S8"
+caps["appPackage"] = "com.quickplay.android.bellmediaplayer.canary"
+caps["appActivity"] = "ca.bell.fiberemote.boot.BootstrapActivity"
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+
+el1 = driver.find_element_by_id("com.quickplay.android.bellmediaplayer.canary:id/dialog_rational_phone_button")
+el1.click()
+el2 = driver.find_element_by_id("com.quickplay.android.bellmediaplayer.canary:id/dialog_rational_location_button")
+el2.click()
+
+driver.quit()
+
+
+
+
+class Appium():
+	def connect(command_executor='http://127.0.0.1:4444/wd/hub'):
+		pass
+	def __init__(command_executor='http://127.0.0.1:4444/wd/hub'):
+		
---
+++ core/appium/webdriver.py
@@ -0,0 +1,19 @@
+
+
+
+class Webdriver(appium_connection):
+	def __init__(self,sessionId):
+		self.sessionId = sessionId
+		params = {'sessionId': self.session_id}
+		
+
+
+	def start_client(self):
+		pass
+
+	def stop_client(self):
+		pass
+
+	def continue_session(sessionId):
+		pass
+
