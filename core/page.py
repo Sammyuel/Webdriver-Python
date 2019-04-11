b44bc4b9a7242141ad9198cd7ab01fae28f7049b
Sam Lee <sam49lee@gmail.com>
2019-04-11 16:42:06 -0400

    Importing page.py

--- core/page.py
+++ core/page.py
@@ -1,5 +1,16 @@
-
+import importlib
 
 class PageFactory():
-	def __init__(self, page):
+	def __init__(self, app_name, **kwargs):
+		self.page = HomePage
+		self.app = app_name
+
+	"""
+	Imports the pages correspending to the self.app name 
+	"""
+	def import_pages():
+		imp_path = from f".apps.{self.app}.pages import * as pages"
+		importlib.import_module(imp_path)
+
+	def page(self):
 
