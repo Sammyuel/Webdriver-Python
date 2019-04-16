12be98d9653116c83eb1234c101456e383a5afbc
Sam Lee <sam49lee@gmail.com>
2019-04-16 15:14:47 -0400

    changes to page factory

--- core/page.py
+++ core/page.py
@@ -4,45 +4,63 @@ import inspect
 
 class PageFactory():
 	def __init__(self, pages, app_name, device, **kwargs):
-		self.pages = import_pages(pages)
+		self.page_classes = page_builder(pages)
+		self.page_objects = {}
 		self.app = app_name
 		self.page = pages.HomePage
 		self.pageVisited = [self.page.__name__]
 
-	def goto(self, page_name):
+	def goto(self, page_name, *args, **kwargs):
 		if hasattr(self.pages, "goto_{}".format(page_name)):
 			page_method = getattr(self.page, "goto_{}".format(page_name))
 			page_method()
 			self.pageVisited.append(self.page.__name__)
-			self.page = getattr(self.pages, page_name)
+			new_page = self.page_objects[page_name] if page_name in page_objects else self.page_classes[page_name](*args, **kwargs)
+			self.page = new_page
 		else:
 			raise Exception("Invalid page name provided to goto method")
+		return self.page
 
 
-	def page_builder(self):
-		for page in self.pages:
-			pass
+	def page_builder(self, modules):
+		page_classes = {}
+		modules = import_page_modules(modules)
+		for module in modules:
+			
 
 
 	def import_page_modules(self, modules):
-		return [m[0] for m in inspect.getmembers(modules, ismodule)]
-
-	def import_page_classes(self, module):
-		return [m[0] for m in inspect.getmembers(module, isclass)]
-
-	def import_page_methods(self, module):
-		return [m[0] for m in inspect.getmembers(module, ismethod)]
+		return {m[0].__name__: m[0] for m in inspect.getmembers(modulesm ismodule)}
 
 
-	def page_class_generator(self, page_name, methods):
+	def page_classes(self, module):
+		methods = [m[0] for m in inspect.getmembers(module, ismethod)]
+		page_classes = {m[0].__name__: m[0] for m in inspect.getmembers(module, isclass)}
+		page_class = type(module.__name__, () , {method.__name__:method for method in methods}) if module.__name__ is in page_classes else None
+		if page_class and page_class.__class__.__name__ not in page_classes: 
+			page_classes[page_class.__class__.__name__] = page_class
+		return page_classes
+
+
+
+	def set_page_objects(self, pages):
+		page_objects = {key:page() for key,page in pages.items()}
+		return page_objects
+
+
+
+	def get_page_classes(self, page_name):
+		methods = import_page_methods(page_name)
+		page_classes = import_page_classes(page_name)
+		if 
+
 		page_class = type(page.__name__, object, {method.__name__:method for method in methods})
 		return page_class
 
 
 
-	def import_
-
-
+	def get_page_objects(page_classes):
+		return {page.__class__.__name__: page for page in page_classes}
 
 
 	"""
