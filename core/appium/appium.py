096b672b1f9849f01a4cf15452b9b17e0f045182
Sam Lee <sam49lee@gmail.com>
2019-04-23 21:47:59 -0400

    appium.py start

--- core/appium/appium.py
+++ core/appium/appium.py
@@ -1,6 +1,21 @@
-from ..core.device import Device
 
+import requests
 
-class Appium(Device):
+class Appium():
+	sessions_url = 'http://0.0.0.0:4723/wd/hub/sessions'
 	def __init__(self, device_info):
 		pass
+
+
+	def get_session_ids(self):
+		r  = requests.get(self.sessions_url)
+		body = dict(r.json())
+		return body
+
+
+example = Appium(5)
+
+body = example.get_session_ids()
+
+print(type(body))
+
